48e7e351de7b83ed483322088894106aa6eb127f
Epic153 <98408550+Epic153@users.noreply.github.com>
2023-11-26 18:09:27 +0700

    Update d.lua

--- d.lua
+++ d.lua
@@ -2,13 +2,13 @@ local Player = game.Players.LocalPlayer
 local Players = game.Players
 local PlayerNames = {}
 
-for _, Player in ipairs(Players) do
+for Player in Players:GetChildren() do
     if Player ~= LocalPlayer then
         table.insert(PlayerNames, Player.Name)
     end
 end
 
 print("Player names:")
-for _, PlayerName in ipairs(PlayerNames) do
-    print(PlayerName)
+for i = 1, #PlayerNames do
+    print(PlayerNames[i])
 end
